d54c7c003dd65c8ffd8ba0b9a7650404b9f5eefb
Marcus Guerrero <marcus.guerrero61@gmail.com>
2025-07-03 22:43:19 +0800

    updating admin table

--- Creating_Table/admin.sql
+++ Creating_Table/admin.sql
@@ -1,10 +1,5 @@
 CREATE TABLE admin(
 	admin_id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR (50),
-    last_name VARCHAR (50),
-    address VARCHAR (100),
-    birth_date DATE,
     email VARCHAR (100),
-    phone_number VARCHAR (50),
     password VARCHAR (50),
     PRIMARY KEY (admin_id));
