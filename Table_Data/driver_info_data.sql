8cdb45fbbc75a9366a62a183b57c9f3505271505
Marcus Guerrero <marcus.guerrero61@gmail.com>
2025-07-04 00:56:35 +0800

    Adding data for driver

---
+++ Table_Data/driver_info_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO driver_info (first_name, last_name, make, model, year, vehicle_type, plate_number, mv_file_number, contact_number, operating, created_by, created_datetime)
+VALUES ("Layla", "Khoury", "Porsche", "911", 1998, "Car", "ZXY 7582", "1305-009876543", "0995-812-3467", "Offline", "Admin", NOW()),
+	   ("Yusuf", "El-Mansouri", "Ducati", "Monster", 2017, "Motorcycle", "TGM 2941", "1103-001245678", "0927-631-2584", "Offline", "Admin", NOW()),
+       ("Eren", "Jager", "Vespa", "GTS", 2008, "Motorcycle", "KLR 8376", "0901-004321987", "0906-734-9812", "Offline", "Admin", NOW()),
+       ("Arjun", "Mehra", "Aventon", "Marlin+", 2024, "E-Bike", "BND 4610", "0202-007654321", "0935-298-4751", "Offline", "Admin", NOW()),
+       ("Omar", "Al-Farouq", "Honda", "Civic", 1992, "Car", "ABC 1234", "1301-000123456", "0917-456-7890", "Offline", "Admin", NOW()),
+       ("Ivan", "Petrov", "MotorStar", "Viber", "2025", "E-Bike", "MQA 1759", "0405-002789456", "0961-807-2349", "Offline", "Admin", NOW()),
+       ("William", "Clarke", "Mitsubishi", "XPander", "2018", "Van", "JUV 9203", "0707-005432198", "0947-519-6830", "Offline", "Admin", NOW()),
+       ("Dmitri", "Sokolov", "Nissan", "NV3500", "2021", "Van", "HXT 3845", "1506-006789321", "0918-625-7943", "Offline", "Admin", NOW()),
+       ("Emily", "Watson", "Tricycle", "Manda", "2015", "Tricycle", "WCP 6102", "1008-003214567", "0992 783 4106", "Offline", "Admin", NOW()),
+       ("Ravir", "Kooper", "Tricycle", "Calooc", "2019", "Tricycle", "LNZ 8457", "0809-008901234", "0908 346 2175", "Offline", "Admin", NOW()),
+       ("Miguel", "Santos", "Reckta", "Cabao", "2025", "Jeep", "KZL 3287", "1303-002456789", "0917 834 5629", "Offline", "Admin", NOW()),
+       ("Andrea", "Dela Cruz", "Antips", "Kaleng", "2014", "Jeep", "MTR 9512", "0807-006712345", "0995 720 3481", "Offline", "Admin", NOW());
